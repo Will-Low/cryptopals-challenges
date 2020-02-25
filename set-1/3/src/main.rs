7a395400423ff8e16763669a416185df8303e791
Will-Low <moose.nugget.music@gmail.com>
2020-02-24 19:23:19 -0500

    Updating #3

--- set-1/3/src/main.rs
+++ set-1/3/src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     let input: Vec<String> = env::args().collect();
     let bytes = hex_to_bytes(&input[1]);
     let xored_vectors = xor_with_letters(&bytes);
-    let 
-    println!("{:?}", xored_vectors);
+    let decrypted_ciphertext = determine_ciphertext(xored_vectors);
+    println!("{:?}", hex::encode(decrypted_ciphertext));
 }
 
 fn hex_to_bytes(input: &String) -> Vec<u8> {
@@ -32,4 +32,24 @@ fn xor_with_letters(bytes: &Vec<u8>) -> Vec<Vec<u8>>{
         xored_vectors.push(xored_letter_vector);
     }
     xored_vectors
+}
+
+fn determine_ciphertext(xored_vectors: Vec<Vec<u8>>) -> Vec<u8> {
+    let mut scores: Vec<(u8, Vec<u8>)> = vec![];
+    for possible_text in xored_vectors {
+        let mut score = 0;
+        for letter in &possible_text {
+            if *letter as char == 'e' || *letter as char == 't' {
+                score = score + 1;    
+            }
+        }
+        scores.push((score, possible_text));
+    }
+    let mut max: (u8, Vec<u8>) = (0, vec![]);
+    for each in scores {
+        if each.0 > max.0 {
+            max = each;
+        }
+    }
+    max.1
 }
