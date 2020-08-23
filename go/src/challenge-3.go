69e22216d04f89375de7384bbde2b988e24fb91f
Will Springer <wspringer@hioscar.com>
2020-08-22 17:57:37 -0700

    challenge 3

--- go/src/challenge-3.go
+++ go/src/challenge-3.go
@@ -4,6 +4,7 @@ import (
     "encoding/hex"
     "fmt"
     "log"
+    "strings"
 )
 
 func main() {
@@ -12,11 +13,7 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-    byteStringToXor := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    bytesToXor, err := hex.DecodeString(byteStringToXor)
-    if err != nil {
-        log.Fatal(err)
-    }
+    bytesToXor := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
     var xoredBytes []([]byte)
 
     for _, char := range bytesToXor {
@@ -26,7 +23,41 @@ func main() {
         }
         xoredBytes = append(xoredBytes, xoredByCharacter)
     }
-    //fmt.Printf("%s\n", xoredBytes) 
-    //xoredString := hex.EncodeToString(xoredBytes)
-   
+    m := make(map[string]int)
+    for _, entry := range xoredBytes {
+        entry := string(entry)
+    // ETAOIN SHRDLU
+        score := 0
+        for _, char := range entry {
+            if strings.ToLower(string(char)) == string("e") {
+                score += 1 
+            } else if strings.ToLower(string(char)) == string("t") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("a") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("o") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("i") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("n") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("s") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("h") {
+                score += 1
+            } else if strings.ToLower(string(char)) == string("r") {
+                score += 1
+            }
+        }
+        m[entry] = score
+    }
+    var maxScore int
+    var maxScoreString string
+    for k, v := range m {
+        if v > maxScore {
+            maxScore = v
+            maxScoreString = k
+        }
+    }
+    fmt.Printf("%s", maxScoreString)
 }
